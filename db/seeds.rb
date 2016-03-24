a5a90159d10683f296f9be808a3c7c8c9eea7561
Patrick Su <afgnsu@gmail.com>
2016-03-24 22:37:48 +0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 puts 'Hello World!'
 puts "這個種子檔會自動建立一個帳號, 並且創建 5 個 groups, 每個 group 各 20 個 posts"
 
-create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', name: '測試用帳號'])
+create_account = User.create([email: 'a@a.a', password: '12345678', password_confirmation: '12345678', name: '測試帳號'])
 
 create_groups = for i in 1..5 do
                   Group.create!([title: "Group no.#{i}", description: "這是用種子建立的第 #{i} 個討論版", user_id: "1"])
